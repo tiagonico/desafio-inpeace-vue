684c4c73316901dce40d08dada42e9c27fb728e5
Tiago Silva <tiagosilva@multicast.com.br>
2022-11-08 20:49:27 -0300

    Creating Project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>HOME</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
