a514e999d8cacdfa4ae00489e70558e1c8b80c2e
Tiago Silva <tiagosilva@multicast.com.br>
2022-11-08 21:17:50 -0300

    Adding NavBar Component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="nav">
+    <img class="nav__img" src="@/assets/logo-uppersoft.png" alt="logo" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+@import "@/scss/colors";
+@import "@/scss/variables";
+.nav {
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+  text-align: center;
+  padding: 0.5rem;
+  background: $colorPrimary;
+
+  &__img{
+    width: 8rem;
+  }
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="home">
+    <NavBar />
     <h1>HOME</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+
+import NavBar from '@/components/NavBar.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    
+    NavBar
   }
 }
 </script>
